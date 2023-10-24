738ac19f9607e6a96ef87602f9ccca19e2a5e61c
T.J. Eckman <jeek@jeek.net>
2023-10-24 04:58:16 -0400

    Buy programs on dark web

--- jeekOS/jeekOS.ts
+++ jeekOS/jeekOS.ts
@@ -2,6 +2,7 @@ import { NS } from "@ns";
 import { Do } from "jeekOS/Do/do";
 import { bootstrap8gb } from "jeekOS/Bootstrap/bootstrap8gb";
 import { Servers } from "jeekOS/Servers/servers";
+import { GetPrograms } from "jeekOS/Darkweb/Programs";
 import { purchasedservers } from "jeekOS/PurchasedServers/purchasedservers";
 
 export async function main(ns: NS): Promise<void> {
@@ -9,6 +10,7 @@ export async function main(ns: NS): Promise<void> {
     await servers.popall();
     servers.popallloop();
     purchasedservers(ns);
+    GetPrograms(ns);
     while (true) {
         await bootstrap8gb(ns);
         await ns.asleep(0);
